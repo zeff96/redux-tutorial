3fa3633968fe1a7acdaee659047798f129d0e96d
zeff96 <zeffo1975@gmail.com>
2023-04-03 17:42:20 +0300

    Add navbar, cartcontainer components and calculate function reducer to the app.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from "./components/NavBar";
+import Cartcontainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotal } from "./redux/features/cart/CartSlice";
+const App = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotal());
+  }, [cartItems]);
+  return (
+    <div>
+      <Navbar />
+      <Cartcontainer />
+    </div>
+  );
+};
+
+export default App;
